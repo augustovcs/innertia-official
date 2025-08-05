c945b30e2c9db984686f653106ba25802e3506fe
Rick <hj.rickcp@gmail.com>
2025-08-05 11:58:06 -0300

    feat: add dashboard layout

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,12 +5,14 @@ import router from '@/routes';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
+import Dashboard from './layouts/Dashboard.vue';
 import Default from './layouts/Default.vue';
 
 const pinia = createPinia();
 
 createApp(App)
   .component('default-layout', Default)
+  .component('dashboard-layout', Dashboard)
   .use(router)
   .use(pinia)
   .mount('#app');
