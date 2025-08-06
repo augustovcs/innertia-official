b5e0a58b4935f6888f2c85cbc4a18ce1d088eedc
Rick <hj.rickcp@gmail.com>
2025-08-06 12:20:13 -0300

    chore(docker): add container reload

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	server: {
+		watch: {
+			usePolling: true,
+		},
 		host: true,
 	},
   plugins: [vue(), tailwindcss()],
