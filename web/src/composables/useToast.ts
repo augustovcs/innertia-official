6239250e790993a433b7927d52f811c61719e33a
Rick <hj.rickcp@gmail.com>
2025-08-06 12:23:34 -0300

    refactor(ui): update toaster style

--- web/src/composables/useToast.ts
+++ web/src/composables/useToast.ts
@@ -6,22 +6,20 @@ export function useToast() {
   const show = (
     status: 'success' | 'error' | 'warning',
     title: string,
-    description = '',
   ) => {
     store.add({
       id: Date.now().toString(),
       status,
       title,
-      description,
     });
   };
 
   return {
-    success: (title: string, description = '') =>
-      show('success', title, description),
-    error: (title: string, description = '') =>
-      show('error', title, description),
-    warning: (title: string, description = '') =>
-      show('warning', title, description),
+    success: (title: string) =>
+      show('success', title),
+    error: (title: string) =>
+      show('error', title),
+    warning: (title: string) =>
+      show('warning', title),
   };
 }
